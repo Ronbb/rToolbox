1bd3ecaa512e12438d5347d5d3bb52e27d609185
Ronbb <ronbiaobiao@gmail.com>
2019-04-11 22:54:26 +0800

    FFmpeg Options define

--- src/renderer/store/modules/_global.js
+++ src/renderer/store/modules/_global.js
@@ -2,7 +2,34 @@ const state = {
   inputFile: null,
   inputFileInfo: null,
   outputFile: null,
-  outputFileInfo: null
+  outputFileInfo: null,
+  // ffmpeg [ global_options ] {[ input_file_options ] -i input_url} ... {[ output_file_options ]output_url} ...
+  ffmpegOptions: {
+    globalOptions: {
+      '-y': null,
+      '-hide_banner': null
+    },
+    inputOptions: {
+      '-ss': null,
+      '-to': null
+    },
+    outputOptions: {
+      /* Video Options */
+      '-r': null, // fps
+      '-s': null, // resolution
+      '-c:v': null, // codec for video
+      '-b:v': null, // bitrate for video, default value is 200K.
+      '-bt': null, // bitrate tolerance: 1-pass mode only for video.
+      '-crf': null,
+      '-pix_fmt': null,
+      '-copyinkf': null,
+      /* Audio Options */
+      '-ar': null, // freq
+      // 'q:a': null,
+      '-b:a': null, // (-ab) bitrate for video, default value is 128K.
+      '-ac': null // channels
+    }
+  }
 }
 
 const mutations = {
